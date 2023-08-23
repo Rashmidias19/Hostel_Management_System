161e9906bf7d2d83f94160a327a1ac1fea5d3fb5
Rashmidias19 <rashmidias819@gmail.com>
2023-08-23 15:30:02 +0530

    login ui created

--- src/lk/ijse/D24/config/SessionFactoryConfig.java
+++ src/lk/ijse/D24/config/SessionFactoryConfig.java
@@ -1,6 +1,6 @@
 package lk.ijse.D24.config;
 
-import lk.ijse.D24.entity.User;
+import lk.ijse.D24.entity.user;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -20,7 +20,7 @@ public class SessionFactoryConfig {
 //              .applyImplicitNamingStrategy(ImplicitNamingStrategyLegacyJpaImpl.INSTANCE)
 //              .build().buildSessionFactory();
 
-        sessionFactory=new Configuration().configure().addAnnotatedClass(User.class)
+        sessionFactory=new Configuration().configure().addAnnotatedClass(user.class)
                 .buildSessionFactory();
     }
 
--- src/lk/ijse/D24/controller/LoginFormController.java
+++ src/lk/ijse/D24/controller/LoginFormController.java
@@ -1,4 +1,28 @@
 package lk.ijse.D24.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.input.MouseEvent;
+
+import java.awt.*;
+
 public class LoginFormController {
+
+    private TextField txtUserName;
+    private PasswordField txtPassword;
+
+    public void btnSignOnAction(ActionEvent event) {
+        if(userCheck()){
+            
+        }
+    }
+
+    private boolean userCheck() {
+    }
+
+    public void txtCreateOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void txtForgotOnAction(MouseEvent mouseEvent) {
+    }
 }
